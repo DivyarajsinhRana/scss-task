ae8ca4d859d90fffcef50d9efbeb8d295ee2198d
Divyarajsinh <divyarajsinh.rana@archesoftronix.com>
2022-01-18 11:43:29 +0530

    second commit

---
+++ scss-task/src/data.js
@@ -0,0 +1,11 @@
+export const data = [{
+    image:"https://www.pngkit.com/png/detail/326-3267989_eukaryotic-cell-eukaryotic-cell-png.png"
+
+},
+{
+    image:"https://miro.medium.com/max/1044/0*c_5KCnFq-zHcpV3B.png"
+
+},
+{
+    image:"https://image.shutterstock.com/image-illustration/single-strand-ribonucleic-acid-rna-260nw-1094792225.jpg"
+}]
